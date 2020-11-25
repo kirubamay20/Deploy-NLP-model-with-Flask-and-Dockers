32de4c544ed8c28d4bd3fc9e11b5909168206d83
KIRUBA KARUPPANNAN <61502293+kirubamay20@users.noreply.github.com>
2020-11-25 14:58:41 +0530

    Update flask_api.py

--- flask_api.py
+++ flask_api.py
@@ -1,3 +1,6 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cluster import kmeans
 from stemming.porter2 import stem
 from flask import Flask, request, make_response, send
 
@@ -14,6 +17,9 @@ def cleanse_text(text):
   else:
     return text
   
+@app.route('/cluster', methods=['POST'])  
+  
+  
 def cluster():
   data = pd.read_csv(request.files['dataset'])
   unstructure = 'text'
@@ -22,7 +28,29 @@ def cluster():
   no_of_clusters = 2
   if 'no_of_clusters' in request.args:
     no_of_clusters = request.args.get('no_of_clusters')
-    
+   
+  data=data.fillna('NULL')
+  data['clean_sum'] = data[unstructure].apply(clean_text)
+  
+  vectorizer= CountVectorizer(analyzer='word', stop_words='english')
+  #Doc1-> He is a lazy boy. she is also lazy
+  #Doc2-> He is a lazy person.
+  #       He she lazy boy person
+  #Doc1   1   1   2    1   0
+  #Doc2   1   0   1    0   1
+  
+  counts = vectorizer.fit_transform(data['clean_sum'])
+  
+  kmeans=KMeans(n_clusters= no_of_clusters)
+  data['cluster_num'] = kmeans.fit_predict(counts)
+  data= data.drop(['clean_sum'],axis=1)
+  
+  return 'commit'
+
+  
+  
+  if __name__ == '__main__':
+    app.run(host='0.0.0.0')
                                 
                                    
 
